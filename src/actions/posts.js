49c84791a56bf880130724050ccd34065a18554c
javierc22 <javier.ec.22@gmail.com>
2021-08-26 15:09:03 -0400

    Delete post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,5 +1,5 @@
 import * as api from '../api/index.js'
-import { FETCH_ALL, CREATE, UPDATE } from '../constants/actionTypes'
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes'
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -23,6 +23,15 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post)
     dispatch({ type: UPDATE, payload: data})
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id)
+    dispatch({ type: DELETE, payload: id})
   } catch (error) {
     console.log(error.message)
   }
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,11 +4,14 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts'
 
 import useStyles from './styles'
 
 const Post = ({post, setCurrentId}) => {
   const classes = useStyles()
+  const dispatch = useDispatch()
 
   return (
     <Card className={classes.card}>
@@ -45,7 +48,11 @@ const Post = ({post, setCurrentId}) => {
         <Button size="small" color="primary">
           <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} 
         </Button>
-        <Button size="small" color="primary">
+        <Button 
+          size="small" 
+          color="primary"
+          onClick={ () => dispatch(deletePost(post._id)) }
+        >
           <DeleteIcon fontSize="small" /> Delete
         </Button>
       </CardActions>
